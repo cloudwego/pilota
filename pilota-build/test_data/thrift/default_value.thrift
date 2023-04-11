b031fb649298a65c7f36dcd8bf5876207ba1829c
Yifei <lyfmagic99@gmail.com>
2023-04-11 14:34:53 +0800

    fix: default value for map, f64, FastStr (#133)

--- pilota-build/test_data/thrift/default_value.thrift
+++ pilota-build/test_data/thrift/default_value.thrift
@@ -3,10 +3,16 @@ enum B {
     Write = 2;
 }
 
+const string A_S = "string";
+
 struct A {
     1: required string faststr = "hello world",
     2: required string string = "test"(pilota.rust_type = "string"),
     3: optional bool a = false,
     4: optional B test_b = B.Read,
     5: optional B test_b2 = 2,
+    6: optional map<string, string> map = {"hello": "world"},
+    7: optional double test_double = 1,
+    8: optional double test_double2 = 1.2,
+    9: optional string alias_str = A_S,
 }
