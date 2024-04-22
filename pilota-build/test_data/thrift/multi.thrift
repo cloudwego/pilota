2bd19e3c1cd217273c59f686e0b24e30cc5cf05a
Millione <isjieliu@163.com>
2024-04-22 16:56:44 +0800

    fix: generate auto derive attributes correctly

--- pilota-build/test_data/thrift/multi.thrift
+++ pilota-build/test_data/thrift/multi.thrift
@@ -1,5 +1,11 @@
 include "default_value.thrift"
+include "recursive_type.thrift"
 
 struct A {
     1: default_value.C c = {"off": "off"},
+}
+
+struct B {
+    1: recursive_type.A a,
+    2: recursive_type.C c, 
 }
