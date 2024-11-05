29a65696882ed2760d2ce501f1a19408a3a8c396
wangjie.wjdew <wangjie.wjdew@bytedance.com>
2024-11-05 18:55:28 +0800

    chore: add separator compatibility test for idl parser

--- pilota-thrift-parser/src/parser/thrift.rs
+++ pilota-thrift-parser/src/parser/thrift.rs
@@ -152,4 +152,31 @@ mod tests {
         "#;
         let (_remain, _res) = File::parse(body).unwrap();
     }
+
+    #[test]
+    fn test_separator() {
+        let body = r#"typedef i32 MyInt32
+typedef string MyString;
+
+struct TypedefTestStruct {
+  1: MyInt32 field_MyInt32;
+  2: MyString field_MyString;
+  3: i32 field_Int32;
+  4: string field_String;
+};
+
+typedef TypedefTestStruct MyStruct,
+
+const list<string> TEST_LIST = [
+    "hello",
+    "world",
+];
+
+service Service {
+  MyStruct testEpisode(1:MyStruct arg)
+},"#;
+        let (remain, res) = File::parse(body).unwrap();
+        assert!(remain.is_empty());
+        assert_eq!(res.items.len(), 6);
+    }
 }
