41909a75d9682a94c3c5555c2a6309c0f944d639
joshua.ho@bytedance.com <joshua.ho@bytedance.com>
2025-08-15 15:38:41 +0800

    fix(pilota-build): remove test files causing errors

--- examples/build.rs
+++ examples/build.rs
@@ -4,17 +4,6 @@ fn main() {
     let out_dir = PathBuf::from(std::env::var("OUT_DIR").unwrap());
     let idl_dir = PathBuf::from(std::env::var("CARGO_MANIFEST_DIR").unwrap()).join("idl");
 
-    // For zero_value
-    let zero_value_idl = idl_dir.join("zero_value.proto");
-    pilota_build::Builder::pb()
-        .ignore_unused(false)
-        .include_dirs(vec![zero_value_idl.parent().unwrap().to_path_buf()])
-        .keep_unknown_fields([zero_value_idl.clone()])
-        .compile_with_config(
-            vec![pilota_build::IdlService::from_path(zero_value_idl)],
-            pilota_build::Output::File(out_dir.join("zero_value.rs")),
-        );
-
     // For fieldmask
     let fieldmask_idl = idl_dir.join("fieldmask.thrift");
     pilota_build::Builder::thrift()
